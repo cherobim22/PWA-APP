73a4cd84b115b934d6ab05af70efffdc1e779451
cherobim22 <lucascherobim@hotmail.com>
2022-05-05 20:09:29 -0300

    fix pwa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pwa example
+
+https://www.youtube.com/watch?v=2zHqTjyfIY8
--- push.js
+++ push.js
@@ -6,8 +6,20 @@ const vapidKeys = {
   privateKey: "zbrPxPwcH1sfI75iV5Npum-wBTFfREgs4m-TpHxw-Z4",
 };
 
-push.setVapidDetails('mailto:teste@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails(
+  "mailto:teste@code.co.uk",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-let sun = {};
-push.sendNotification(sub,  'test message')
-
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dFWK5cN3BKs:APA91bFdpH0VeOiEhWp29femiFN787nz6-wut49i6u74hm-8gaLnpfzxdaRYTYcmNn3LM-0Itoi5B9VwwvYkHZdsKJIHkqEuq2KFET4hV-ryjqTArCSwXIzqHM4WpFS4Rm_ch8hbgrI6",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BEMkWbXDuVmY_Gzy8AVW6dY2fyYrPmjke_o6aakgOD2c-ToQPmljHd_KUDnfqQfvCdHqKjBKdAdMXAaLA83pV00",
+    auth: "EDtjiaTioN8xLGxXUnzskg",
+  },
+};
+push.sendNotification(sub, "test message");
