a8c8f66b30de850a54e73096d21046d38f741286
cherobim22 <lucascherobim@hotmail.com>
2022-05-05 20:44:27 -0300

    fix pwa

--- push.js
+++ push.js
@@ -1,5 +1,9 @@
 let push = require("web-push");
 
+function log(sub) {
+  console.log({sub});
+}
+
 const vapidKeys = {
   publicKey:
     "BHMt9biPRmvq_4HEduy-T2Pe6dBdLDk7JF0DvIIy1RMgy0foiB-Wrp3CSKG3a1gqIFs3bsKHSfT7pQCZ1fLVinQ",
@@ -7,19 +11,20 @@ const vapidKeys = {
 };
 
 push.setVapidDetails(
-  "mailto:teste@code.co.uk",
+  "mailto:cherobim22@gmail.com",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 let sub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/dFWK5cN3BKs:APA91bFdpH0VeOiEhWp29femiFN787nz6-wut49i6u74hm-8gaLnpfzxdaRYTYcmNn3LM-0Itoi5B9VwwvYkHZdsKJIHkqEuq2KFET4hV-ryjqTArCSwXIzqHM4WpFS4Rm_ch8hbgrI6",
+    "https://fcm.googleapis.com/fcm/send/f43v3ZionQ4:APA91bFRNqKIFMEb1FLuLmeqQxxktAqL_E8leJxcMOYH3n2HfQqKyGZGnnrAVp_ASJfi0lzmfPoJgAym0lH9092IMOvQ0mLKtRZVgVv2KGYkYMU8estXdaFHUZyksJrNhhShPKqpJsH4",
   expirationTime: null,
   keys: {
     p256dh:
-      "BEMkWbXDuVmY_Gzy8AVW6dY2fyYrPmjke_o6aakgOD2c-ToQPmljHd_KUDnfqQfvCdHqKjBKdAdMXAaLA83pV00",
-    auth: "EDtjiaTioN8xLGxXUnzskg",
+      "BJFtBhgeGdNDDn1A1Wn0yk3f_tlKJ__CbtWjMZJXoXzRM0Sn9BmCjQojVxuuGcDaD5nSYft01680Rm7bW2THNwc",
+    auth: "AexcPLy9wBzZuJ_6PVPALQ",
   },
 };
 push.sendNotification(sub, "test message");
+log(sub)
